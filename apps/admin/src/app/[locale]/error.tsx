5355b8cec6e1a0b3e2689636e8bd1286e17758ef
Park Bomin <parkbm@cle.vision>
2025-06-02 19:06:30 +0900

    test: husky

--- apps/admin/src/app/[locale]/error.tsx
+++ apps/admin/src/app/[locale]/error.tsx
@@ -8,7 +8,7 @@ export default function Error() {
   const router = useRouter();
   const t = useTranslations('error');
 
-  const a;
+  const ab;
 
   return (
     <div className='flex flex-1 flex-col items-center justify-center gap-12'>
