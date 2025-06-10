3cf0d29673301ba1b87347b5185e99276c0bba82
Park Bomin <parkbm@cle.vision>
2025-06-10 22:03:18 +0900

    feature: /graphql 리디렉션 추가

--- apps/admin_nest/src/app.controller.spec.ts
+++ apps/admin_nest/src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should trigger a redirect to /graphql', () => {
+      expect(() => {
+        appController.redirectToGraphQL();
+      }).not.toThrow();
     });
   });
 });
--- apps/admin_nest/src/app.controller.ts
+++ apps/admin_nest/src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// app.controller.ts
+import { Controller, Get, Redirect } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect('/graphql')
+  redirectToGraphQL() {
+    return;
   }
 }
