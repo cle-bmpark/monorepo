6b82671c3e15d4ab2599226b60cdc2dade970a0c
Park Bomin <parkbm@cle.vision>
2025-06-11 11:12:26 +0900

    fix: [admin_nest] tsconfig-paths/register 런타임용으로 변경

--- apps/admin_nest/src/main.ts
+++ apps/admin_nest/src/main.ts
@@ -1,7 +1,6 @@
 import { AppModule } from '@/app.module';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import 'tsconfig-paths/register';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
