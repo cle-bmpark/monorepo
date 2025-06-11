728bffcdd3f0903df9141a02dbf378992442325a
Park Bomin <parkbm@cle.vision>
2025-06-11 11:08:57 +0900

    fix: [admin_nest] 절대경로 node.js 에서 읽지 못하는 버그

--- apps/admin_nest/src/main.ts
+++ apps/admin_nest/src/main.ts
@@ -1,6 +1,7 @@
+import { AppModule } from '@/app.module';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+import 'tsconfig-paths/register';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
