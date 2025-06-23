3300441d9238e44534e67fee2a87d73f2f078716
Park Bomin <parkbm@cle.vision>
2025-06-23 16:15:38 +0900

    feature: [issue_next] ID 조회 페이지 - id 노출 추가 delete: [issue_next] NoData 버튼 삭제

--- apps/issue_next/src/app/[locale]/(public)/result/[filter]/Id.tsx
+++ apps/issue_next/src/app/[locale]/(public)/result/[filter]/Id.tsx
@@ -9,13 +9,15 @@ import Error from '@/app/[locale]/error';
 import Loading from '@/app/[locale]/loading';
 import ItemBox from '@/components/itemBox';
 import { useGetItemsByIdQuery } from '@/graphql/generated/graphql';
-
+import useClipboard from '@/hooks/common/useClipboard';
+import LinkButton from '@repo/ui/src/components/button/LinkButton';
 interface IdProps {
   id: string;
 }
 
 export default function Id({ id }: IdProps) {
   const t = useTranslations('result');
+  const copyToClipboard = useClipboard();
 
   const { data, error, loading } = useGetItemsByIdQuery({
     variables: { id: id },
@@ -28,6 +30,15 @@ export default function Id({ id }: IdProps) {
     <div className='flex flex-1 flex-col'>
       {data?.items && data.items.length > 0 ? (
         <div className='shadow-light flex flex-col gap-4 p-4'>
+          <div className='flex items-center'>
+            <span className='text-grey-600'>ID&nbsp;</span>
+            <LinkButton
+              value={data.items[0]?.id ?? ''}
+              onClick={() => {
+                void copyToClipboard('ID', data.items?.[0]?.id ?? '');
+              }}
+            />
+          </div>
           <p className='font-700'>{data.items[0]?.name}</p>
 
           <div className='flex flex-col gap-3'>
--- apps/issue_next/src/app/[locale]/(public)/result/[filter]/NoData.tsx
+++ apps/issue_next/src/app/[locale]/(public)/result/[filter]/NoData.tsx
@@ -1,24 +1,11 @@
-import Button from '@ui/components/button/Button';
 import { useTranslations } from 'next-intl';
-import { useRouter } from 'next/navigation';
-import { IoSearch } from 'react-icons/io5';
 
 export default function NoData() {
   const t = useTranslations('result');
-  const router = useRouter();
 
   return (
     <div className='flex flex-1 flex-col items-center justify-center gap-4'>
       <p className='text-grey-800 text-center'>{t('noDataById')}</p>
-      <div className='flex w-full'>
-        <Button
-          value={t('research')}
-          onClick={() => router.push('/main/search')}
-          style='secondary'
-          isIcon
-          icon={<IoSearch />}
-        />
-      </div>
     </div>
   );
 }
