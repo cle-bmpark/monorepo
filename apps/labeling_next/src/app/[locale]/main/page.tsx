3e76d88b074a1e278ec976ec6826ecc5e3c3f917
Park Bomin <parkbm@cle.vision>
2025-06-24 17:54:41 +0900

    feature: [labeling_next] 첫 화면 입장 시, 폴더 자동 선택

--- apps/labeling_next/src/app/[locale]/main/page.tsx
+++ apps/labeling_next/src/app/[locale]/main/page.tsx
@@ -4,21 +4,31 @@ import Header from '@/app/[locale]/main/(header)/Header';
 import SelectedImage from '@/app/[locale]/main/(image)/SelectedImage';
 import ImageSidebar from '@/app/[locale]/main/(imageSidebar)/ImageSidebar';
 import ObjectSidebar from '@/app/[locale]/main/(objectSidebar)/ObjectSidebar';
-import { ImageFileType, ImageFolderType, useSelectedImageQuery } from '@/hook/query/image';
-import { useState } from 'react';
+import {
+  ImageFileType,
+  ImageFolderType,
+  useImageFoldersQuery,
+  useSelectedImageQuery,
+} from '@/hook/query/image';
+import { useEffect, useState } from 'react';
 
 export default function Page() {
   const [selectedFolder, setSelectedFolder] = useState<ImageFolderType | undefined>();
   const [selectedImage, setSelectedImage] = useState<ImageFileType | undefined>();
 
-  const { data } = useSelectedImageQuery(selectedFolder?.name, selectedImage?.name);
+  const { data: folder } = useImageFoldersQuery();
+  const { data: image } = useSelectedImageQuery(selectedFolder?.name, selectedImage?.name);
+
+  useEffect(() => {
+    setSelectedFolder(folder?.[0]);
+  }, [folder]);
 
   return (
     <div className='flex flex-1 flex-col'>
       <Header selectedFolder={selectedFolder} setSelectedFolder={setSelectedFolder} />
       <div className='flex flex-1 justify-between'>
-        <ObjectSidebar data={data?.objects} />
-        <SelectedImage data={data} />
+        <ObjectSidebar data={image?.objects} />
+        <SelectedImage data={image} />
         <ImageSidebar
           selectedFolder={selectedFolder}
           selectedImage={selectedImage}
