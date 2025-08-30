198c2155b935bbaacb72f5d7712b5109c0913011
Park Bomin <parkbm@cle.vision>
2025-08-30 18:40:54 +0900

    refactor: 프로그레스 모달창 width 속성 추가

--- packages/ui/src/components/modal/ProgressModal.tsx
+++ packages/ui/src/components/modal/ProgressModal.tsx
@@ -28,19 +28,19 @@ export default function ProgressModal() {
   return (
     <div className='fixed top-0 right-0 bottom-0 left-0 z-20 bg-grey-950/40'>
       <div
-        className='absolute top-1/2 left-1/2 flex flex-1 -translate-x-1/2 -translate-y-1/2 transform items-center justify-between gap-10 rounded-lg border border-grey-700 bg-grey-0 px-8 py-6 shadow-strong'
+        className={`absolute top-1/2 left-1/2 flex flex-1 -translate-x-1/2 -translate-y-1/2 transform items-center justify-between gap-10 rounded-lg border border-grey-700 bg-grey-0 px-8 py-6 shadow-strong ${progress.width ?? 'w-[800px]'}`}
         ref={modalRef}
       >
-        <div className='flex flex-col gap-4'>
+        <div className='flex w-full flex-col gap-4'>
           <div className='flex items-center gap-2'>
             <h3>{progress.title}</h3>
             <p className='text-14 leading-20 font-400 text-grey-700'>
               ({progress.count}/{progress.total})
             </p>
           </div>
-          <div className='relative flex h-1.5 w-[416px] rounded-full bg-grey-300'>
+          <div className='relative flex h-1.5 w-full rounded-full bg-grey-300'>
             <div
-              className={`absolute top-0 left-0 z-20 h-1.5 rounded-full bg-primary-500`}
+              className={`absolute top-0 left-0 z-20 h-1.5 w-full rounded-full bg-primary-500`}
               style={{ width: `${416 * (progress.count / progress.total)}px` }}
             />
           </div>
