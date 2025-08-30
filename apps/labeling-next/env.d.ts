ac2823d4de3c37b85f36f80c635a759a9070eb53
Park Bomin <parkbm@cle.vision>
2025-08-30 16:05:23 +0900

    delete: 라벨링 툴 웹 프로젝트 삭제

--- apps/labeling-next/env.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NEXT_PUBLIC_API_HOST: string;
-    readonly NEXT_PUBLIC_API_URL: string;
-  }
-}
--- apps/labeling-next/src/app/[locale]/main/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function Layout({
-  children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
-  return <div className='flex flex-1'>{children}</div>;
-}
